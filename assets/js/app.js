659db1871ff22dc46b0a93984f8c8f51db9912f8
dlmponda <dlmpondafx@gmail.com>
2020-04-21 16:50:07 +0200

    i added a js file with an event listener on the submit button which calls the validation function which i also defined

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+ 
+const validation = (e) =>{
+    e.preventDefault()
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const ISBN = document.querySelector("#ISBN").value;
+}
+
+// 01 Event Listener 
+ document.querySelector("form").addEventListener("submit", validation)
+
+
+
+
+
+
+
+
+
+
+
+
