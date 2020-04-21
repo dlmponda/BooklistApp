60ed6eb1e63b034160057b4dcb373b73fc497150
dlmponda <dlmpondafx@gmail.com>
2020-04-21 18:47:19 +0200

    i added an if statement,showAlert message function to validate all input plus its respective css styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,41 @@
  
+// custom method 
+const method = {
+    showAlert: (message, className) => {
+        const alterMessage = document.createElement("div");
+        alterMessage.className = `alert ${className}`;
+        alterMessage.appendChild(document.createTextNode(message));
+       
+        const section = document.querySelector("section");
+        const main = document.querySelector("main");
+        section.insertBefore(alterMessage,main);
+        
+        // Vanish in 3 seconds 
+        setTimeout( function() {
+            alterMessage.className = 'remove';
+        },2000);
+
+        setTimeout(()=> document.querySelector('.remove').remove(),3500)
+    }
+}
+
 const validation = (e) =>{
     e.preventDefault()
     const title = document.querySelector("#title").value;
     const author = document.querySelector("#author").value;
     const ISBN = document.querySelector("#ISBN").value;
+
+    if (title === "" || author === "" || ISBN === "") {
+        method.showAlert('Please fill in all fields', 'redMessage')
+       
+    } else {
+        
+       
+    }
+
 }
 
-// 01 Event Listener 
+// 01 Event Listener
  document.querySelector("form").addEventListener("submit", validation)
 
 
